a22ef062108281fcf4916affe7bd07ee7e2ee7b2
Devin Flake <flakeman2@hotmail.com>
2019-12-17 22:06:21 -0700

    saving work, lcr-sms.py still under construction

--- lcr-sms.py
+++ lcr-sms.py
@@ -31,9 +31,9 @@ def main(args):
     """
     This script is meant to be run from the cli
     """
+    dnc = 'do_not_contact.txt'
     config_file = '.creds'
-    account_sid = ''
-    auth_token = ''
+    do_not_contact = []
     phone_list = []
 
     cli = argparse.ArgumentParser(description="Send an SMS text using the Twilio API")
@@ -55,26 +55,18 @@ def main(args):
                 \nExiting.")
         exit(2)
 
-    if os.path.isfile(opts.list):
-        with open(opts.list) as phone_file:
-            for line in phone_file:
+    if os.path.isfile(dnc):
+        with open(dnc) as do_not:
+            for line in do_not:
                 line = line.split('#', 1)[0]
-                line = re.sub('[-().+]', '', line)
+                line = re.sub('[-().+ ]', '', line)
                 if line:
-                    phone_list.append(line.strip())
-    # this could be either one phone number or a bad path/file.
-    #elif len(re.sub('[-().+]', '', opts.list)) < 10:
-    elif opts.list.isnumeric():
-        line = re.sub('[-().+]', '', opts.list)
-        phone_list = line.split(' ')
-    else:
-        print("\nFile not found. Exiting.\n")
-        exit(2)
-
-    phone_list = list(filter(None, phone_list))
-
+                    if line.startswith("1"):  # If phone number starts with 1, remove it
+                        line = line[1:]
+                    do_not_contact.append(line.strip())
+    
     if not os.path.exists(config_file):
-        print(f'{config_file} file not found! Exiting.')
+        print(f'{config_file} file not found!  Exiting.')
         exit(2)
 
     # Get twilio account and auth info from config file
@@ -104,7 +96,44 @@ def main(args):
             pieces = line.split('=')
             country_code = pieces[1]
 
+    try:
+        for config_setting in [account_sid, auth_token, twilio_num, username, password, ward_unit, country_code]:
+            config_setting
+    except UnboundLocalError:
+        print(f'A config setting is not set in the {config_file} file.\nPlease check that you have a setting for:\n\
+        account_sid, auth_token, twilio_num, username, password, ward_unit, and country_code.')
+        exit(2)
+
+    lcr = LCR(username, password, ward_unit)
+
+    count = 0
+    months = 1
+    members_alt = lcr.members_alt()
+    print('len members_alt', len(members_alt))
+    #print(members_alt)
+    for member in members_alt:
+        if opts.org == 'eq':
+            if member['priesthoodOffice'] != 'NONE':
+                #print(member)
+                print(member['nameFormats'].get('listPreferredLocal'), member['nameFormats'].get('givenPreferredLocal'), member['priesthoodOffice'], member['phoneNumber'])
+                #print('---------------------------------------')
+                count += 1
+                #if count == 6:
+                #    exit()
+
+        elif opts.org == 'rs':
+            pass
+        elif opts.org == 'all':
+            pass
+        else
+            print('Unrecognized organization!  Exiting.')
+            exit(2)
+    
+    print('count', count)
+    exit()
+
     client = Client(account_sid, auth_token)
+    #phone_list = list(filter(None, phone_list))
 
     logging.basicConfig(filename='lcr-sms.log', filemode='a',\
             format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',\
@@ -120,22 +149,23 @@ def main(args):
     print('')
 
     for phone_num in phone_list:
-        phone_num = phone_num.replace(" ", "")
+        phone_num = re.sub('[-().+ ]', '', phone_num)
         if phone_num.startswith("1"): # If phone_num starts with 1 remove it
             phone_num = phone_num[1:]
-        message = client.messages \
-            .create(
-                body=opts.body,
-                from_='+'+country_code+twilio_num, # Your twilio number. Costs about $1/month, $0.0075/SMS
-                to='+'+country_code+phone_num      # +1 is the USA country code, adjust for your location
-            )
-        
-        output = f"phone_num = {phone_num} ; body = \"{opts.body}\" ; {message.sid}"
-        logging.info(output)
-        if opts.verbose:
-            print(output)
-
-        #exit()
+        if phone_num not in do_not_contact:
+            message = client.messages \
+                .create(
+                    body=opts.body,
+                    from_='+'+country_code+twilio_num, # Your twilio number. Costs about $1/month, $0.0075/SMS
+                    to='+'+country_code+phone_num      # +1 is the USA country code, adjust for your location
+                )
+            
+            output = f"phone_num = {phone_num} ; body = \"{opts.body}\" ; {message.sid}"
+            logging.info(output)
+            if opts.verbose:
+                print(output)
+    
+            #exit()
 
     print("Done.")
 
